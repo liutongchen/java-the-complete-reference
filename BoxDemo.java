83d698bfa3414b6e4511f318195b16d11703f5d9
Liutong Chen <liutong.clt@gmail.com>
2018-03-13 09:44:59 -0700

    Using classes.

---
+++ BoxDemo.java
@@ -0,0 +1,22 @@
+//Chapter 6: Introducing Classes
+
+class Box {
+    double width;
+    double height;
+    double depth;
+}
+
+class BoxDemo {
+    public static void main(String args[]) {
+        Box myBox = new Box();
+        double vol;
+
+        myBox.width = 10;
+        myBox.height = 20;
+        myBox.depth = 15;
+
+        vol = myBox.width * myBox.height * myBox.depth;
+
+        System.out.println("Volumn is " + vol);
+    }
+}
