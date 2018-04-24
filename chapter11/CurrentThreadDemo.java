3cb0fe8948019dd9d36de3b82a4741e6a9298a7f
Liutong Chen <liutong.clt@gmail.com>
2018-04-24 13:33:09 -0700

    Create thread through implementing Runnable.

--- chapter11/CurrentThreadDemo.java
+++ chapter11/CurrentThreadDemo.java
@@ -1,15 +1,39 @@
+class NewThread implements Runnable {
+    // create thread through implementing Runnable
+    private Thread t;
+
+    NewThread() {
+        t = new Thread(this, "Demo Thread");
+        System.out.println("Child thread: " + t);
+    }
+
+    public Thread getNewThread() {
+        return t;
+    }
+
+    // This is the entry point for the second thread.
+    public void run() {
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("Child Thread: " + i);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Child interrupted.");
+        }
+        System.out.println("Existing child thread.");
+    }
+}
+
 class CurrentThreadDemo {
     public static void main (String[] args) {
-        Thread t = Thread.currentThread();
-        System.out.println("Current thread: " + t);
+        NewThread nt = new NewThread();
 
-        // change the name of the thread
-        t.setName("My Thread");
-        System.out.println("After the name changed: " + t);
+        nt.getNewThread().start();
 
         try {
             for (int n = 5; n > 0; n--) {
-                System.out.println(n);
+                System.out.println("Main Thread: " + n);
                 Thread.sleep(1000);
             }
         } catch (InterruptedException e) {
