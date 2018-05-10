ff0c8809ccbddd0c90bc9bcde40385b51cfa5973
Liutong Chen <liutong.clt@gmail.com>
2018-05-10 11:26:26 -0700

    Synchronized calls.

---
+++ chapter11/SyncThreadDemo.java
@@ -0,0 +1,58 @@
+class Callme {
+    void call(String msg) {
+        System.out.println("[" + msg);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        System.out.println("]");
+    }
+}
+
+class Caller implements Runnable {
+    String msg;
+    Callme target;
+    Thread t;
+
+    public Caller(Callme targ, String s) {
+        target = targ;
+        msg = s;
+        t = new Thread(this);
+    }
+
+    // synchronize calls to call()
+    public void run() {
+        synchronized(target) {
+            target.call(msg);
+        }
+    }
+
+    // unsynchronized calles
+//    public void run() {
+//        target.call(msg);
+//    }
+}
+
+class SyncThreadDemo {
+    public static void main(String args[]) {
+        Callme target = new Callme();
+        Caller obj1 = new Caller(target, "Hello");
+        Caller obj2 = new Caller(target, "Synchronized");
+        Caller obj3 = new Caller(target, "World");
+
+        // Start the thread
+        obj1.t.start();
+        obj2.t.start();
+        obj3.t.start();
+
+        // wait for threads to end
+        try {
+            obj1.t.join();
+            obj2.t.join();
+            obj3.t.join();
+        } catch(InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+    }
+}
